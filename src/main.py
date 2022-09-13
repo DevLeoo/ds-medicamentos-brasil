46dc12a7939b9dc2795047bd1ea920fd5fcb4138
Leonardo Teixeira <leonardo.leotc@gmail.com>
2022-09-13 11:54:37 -0300

    feat: deploy

--- src/main.py
+++ src/main.py
@@ -172,4 +172,4 @@ hide_streamlit_style = """
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
